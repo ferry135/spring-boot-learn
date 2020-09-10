32a64206d422e903b54fa4113fa552b49d18f93e
zengweijin <1351072964@qq.com>
2020-09-10 15:24:35 +0800

    HelloWord

---
+++ spring-boot-learn-helloword/src/main/java/com/zwjlearn/helloword/SpringBootHelloWordApplication.java
@@ -0,0 +1,20 @@
+package com.zwjlearn.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class SpringBootHelloWordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWordApplication.class,args);
+    }
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+}
